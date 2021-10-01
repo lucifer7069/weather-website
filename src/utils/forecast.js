ed0a15d9a1b102cb8c32714255b70f72b729e018
yash <yashkalariya36@gmail.com>
2021-10-01 12:46:10 +0530

    update in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude , longitude , callback ) => {
         } else if (body.error) {
             callback('Unable To Find Location!' , undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
